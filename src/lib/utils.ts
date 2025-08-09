204b1850fab2de793df66f7132bd8cb7318d14d0
mThanuj <mullagurithanuj0@gmail.com>
2025-08-09 15:34:03 +0530

    add expiresAt utility function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
 }
+
+export const getExpiryDate = (daysToAdd: number): Date => {
+    const today = new Date();
+
+    today.setHours(0, 0, 0, 0);
+
+    const futureDate = new Date(today);
+    futureDate.setDate(today.getDate() + daysToAdd);
+
+    return futureDate;
+};
